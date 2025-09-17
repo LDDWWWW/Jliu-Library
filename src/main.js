66ac2c6b30619872b2deb94d2bc294be9f5a9a4e
LDDWWWW <612313@qq>
2025-09-17 15:33:36 +0800

    Week 7 Finished

--- src/main.js
+++ src/main.js
@@ -6,9 +6,20 @@ import router from './router'
 
 import PrimeVue from 'primevue/config'
 import Aura from '@primevue/themes/aura'
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC5s6SUrvRm8uLImYiNvQPSFFZqrAbhUr4",
+  authDomain: "week7-junchen.firebaseapp.com",
+  projectId: "week7-junchen",
+  storageBucket: "week7-junchen.firebasestorage.app",
+  messagingSenderId: "39343364463",
+  appId: "1:39343364463:web:fa70ad4b3369326e65709a"
+};
 
 
 const app = createApp(App)
+initializeApp(firebaseConfig)
 app.use(PrimeVue, { theme: { preset: Aura } })
 app.use(router)
 
