df959192d141eaceb5f9e9999cf112849ec04aba
LDDWWWW <612313@qq>
2025-10-27 10:40:19 +0800

    trigger commit

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/Jliu-Library/'
-    : '/'
+  base: '/Jliu-Library/'
 })
